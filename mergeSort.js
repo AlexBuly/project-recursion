a6c1d383e8733ee4a546a21e16e31467feaface4
Alex Buly <asbuly@icloud.com>
2024-10-25 13:31:39 -0400

    Adds mergeSort.js

---
+++ mergeSort.js
@@ -0,0 +1,10 @@
+function mergeSort([...array]) {
+    /* if array not sorted
+            split array
+            sort the left half
+        if left sorted 
+            sort right half
+        if left and right sorted
+            merge sorted half
+    */
+}
