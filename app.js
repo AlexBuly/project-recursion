a8288107993d66662fdfcb7f5d47fd9ac1e94846
Alex Buly <asbuly@icloud.com>
2024-10-25 13:25:27 -0400

    Updates fibs function

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 function fibs(num) {
     let fibsArray = [0, 1];
 
-    for (let i = 1; i < num - 1; i++) {
-        const fibNum = fibsArray.at(-2) + fibsArray.at(-1);
+    if (num <= 0) return fibsArray;
+    if (num === 1) return [0];
+
+
+
+    for (let i = 2; i < num; i++) {
+        const fibNum = fibsArray[i - 2] + fibsArray[i - 1];
         fibsArray.push(fibNum);
     }
-    console.log(fibsArray);
+   console.log(fibsArray)
 }
+
+fibs(3);
+
