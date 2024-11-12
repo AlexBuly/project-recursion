945e1e8965abb2f3bee950f0df019d98ff13303b
Alex Buly <asbuly@icloud.com>
2024-11-12 07:21:06 -0500

    Adds comments to fibonacci

--- fib.js
+++ fib.js
@@ -14,13 +14,18 @@ function fibs(num) {
    console.log(fibsArray)
 }
 
+fibs(8);
+
 function fibsRec(num, fibsArray = [0, 1]) {
     console.log("This was printed recursively");
     if (num <= fibsArray.length) {
+    // slice from 0 up until number
      return fibsArray.slice(0, num);
     }
  
     const fibNum = fibsArray[fibsArray.length - 2] + fibsArray[fibsArray.length - 1];
+
+    // append new fibNum to fibsArray
     return fibsRec(num, [...fibsArray, fibNum]);
  }
  
